e4716cb393043dfec5130c53c0fd6b9a5be757ac
Peter Poo Yi Sing <pooyisingwork@gmail.com>
2018-01-18 02:51:08 +0800

    Changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,29 @@
 
 Admin.create(email: 'admin@gmail.com', password: 'password')
 
-Brand.create(name: 'Kat Von D Beauty')
-Brand.create(name: 'Tarte')
-Brand.create(name: 'Pixi')
-Brand.create(name: 'Sephora')
+Brand.create(name: 'Gundam Build Fighters')
+Brand.create(name: 'Gundam: The Origin')
+Brand.create(name: 'Gundam Unicorn')
+Brand.create(name: 'Gundam Destiny Astray')
+Brand.create(name: 'Gundam X')
+Brand.create(name: 'Gundam SEED Destiny')
+Brand.create(name: 'Gundam: Counterattack')
 
-Category.create(name: 'Face')
-Category.create(name: 'Lips')
+Category.create(name: 'Gundam')
+Category.create(name: 'Zaku')
 
-Product.create(name: 'Lightning Black Warrior', brand_id: 4, cost_price: 3 , selling_price: 11, stock: 10, images_attributes: [{url: 'https://i0.wp.com/www.gunpla101.com/wp-content/uploads/2017/10/black-warrior.jpg?resize=1200%2C675&ssl=1', position: 1}], description: ' Lightning Black Warrior')
+Product.create(name: 'PF-73-3BL Lightning Warrior Gundam', category_ids: 1, brand_id: 1, cost_price: 3 , selling_price: 32, stock: 10, images_attributes: [{url: 'http://cdn.shopify.com/s/files/1/0736/6567/products/gundam-build-fighters-battlogue-high-grade-1-144-plastic-model-gundam-black-lightning-warrior_HYPETOKYO_1_grande.jpg?v=1501479346', position: 1}], description: 'PF-73-3BL Lightning Warrior Gundam')
 
-Product.create(name: 'Black Tri-Stars Zaku II Hi Maneuver Type', brand_id: 3, cost_price: 22 , selling_price: 59, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/51WcP-aDgFL.jpg', position: 1}], description: 'Black Tri-Stars Zaku II Hi Maneuver Type' )
+Product.create(name: 'Black Tri-Stars Zaku II Hi Maneuver Type', category_ids: 2, brand_id: 2, cost_price: 37 , selling_price: 38, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/51WcP-aDgFL.jpg', position: 1}], description: 'Black Tri-Stars Zaku II Hi Maneuver Type' )
 
-Product.create(name: 'RX-0 Unicorn Gundam', brand_id: 1, cost_price: 15 , selling_price: 31, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/41kFC8s6lzL._SY450_.jpg', position: 1}], description: 'RX-0 Unicorn Gundam' )
+Product.create(name: 'RX-0 Unicorn Gundam', category_ids: 1, brand_id: 3, cost_price: 15 , selling_price: 45, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/41kFC8s6lzL._SY450_.jpg', position: 1}], description: 'RX-0 Unicorn Gundam' )
 
-Product.create(name: 'Gundam Astray Red Frame', brand_id: 1, cost_price: 19, selling_price: 40, stock: 10, images_attributes: [{url: 'https://i5.walmartimages.com/asr/54dd4608-af0b-439d-be5e-c383a4469e06_1.b9a8460697d510989aa79feca4869942.jpeg', position: 1}], description: 'Gundam Astray Red Frame' )
+Product.create(name: 'MBF-P02 Astray Red Frame Gundam', category_ids: 1, brand_id: 4, cost_price: 19, selling_price: 40, stock: 10, images_attributes: [{url: 'https://i5.walmartimages.com/asr/54dd4608-af0b-439d-be5e-c383a4469e06_1.b9a8460697d510989aa79feca4869942.jpeg', position: 1}], description: 'MBF-P02 Astray Red Frame Gundam' )
 
-Product.create(name: 'Gundam X Devil', brand_id: 2, cost_price: 20, selling_price: 49, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/710azzoKoHL._SY550_.jpg', position: 1}], description: 'Gundam X Devil' )
+Product.create(name: 'GX-9900 X Devil Gundam', category_ids: 1, brand_id: 5, cost_price: 20, selling_price: 49, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/710azzoKoHL._SY550_.jpg', position: 1}], description: 'GX-9900 X Devil Gundam' )
 
-Product.create(name: 'Gundam Double X', brand_id: 2, cost_price: 8, selling_price: 26, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/71aIE2W8aKL._SL1203_.jpg', position: 1}], description: 'Gundam Double X' )
+Product.create(name: 'GX-9901-DX Double X Gundam', category_ids: 1, brand_id: 5, cost_price: 8, selling_price: 37, stock: 10, images_attributes: [{url: 'https://images-na.ssl-images-amazon.com/images/I/71aIE2W8aKL._SL1203_.jpg', position: 1}], description: 'GX-9901-DX Double X Gundam' )
 
-Product.create(name: 'Customized Zaku II Model', brand_id: 3, cost_price: 15, selling_price: 39, stock: 10, images_attributes: [{url: 'https://cdn.shopify.com/s/files/1/0736/6567/products/gundam-seed-master-grade-1-100-plastic-model-zgmf-x13a-providence-gundam-g-u-n-d-a-m-premium-edition_HYPETOKYO_1_1024x1024.jpg?v=1486865949, position: 1}], description: "Customized Zaku II Model" )
+Product.create(name: 'ZGMF-X666S Legend Gundam', category_ids: 1, brand_id: 6, cost_price: 15, selling_price: 55, stock: 10, images_attributes: [{url: 'https://cdn.shopify.com/s/files/1/0736/6567/products/gundam-seed-master-grade-1-100-plastic-model-zgmf-x13a-providence-gundam-g-u-n-d-a-m-premium-edition_HYPETOKYO_1_1024x1024.jpg?v=1486865949', position: 1}], description: "ZGMF-X666S Legend Gundam" )
+
+Product.create(name: 'RX-93 V GUNDAM', category_ids: 1, brand_id: 7, cost_price: 8, selling_price: 51, stock: 10, images_attributes: [{url: 'https://images-fe.ssl-images-amazon.com/images/I/417WTa3ZNxL._AA500_.jpg', position: 1}], description: 'RX-93 V GUNDAM' )
