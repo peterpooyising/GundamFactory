80bf2fe780060864f2f7b48185874821ba697f6b
Peter Poo Yi Sing <pooyisingwork@gmail.com>
2018-01-19 15:55:33 +0800

    added placeholder values with customer account.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Customer.create(email: 'customer@gmail.com', password: 'password')
 Admin.create(email: 'admin@gmail.com', password: 'password')
 
 Brand.create(name: 'Gundam Build Fighters')
